21895b024ba6faf03d00b8a618c12d3595853613
Kristoffer Cabulong <kcabulon@email.arizona.edu>
2015-02-06 11:55:38 -0700

    Created MVC packages for dispatch. Created foundations for interacting with server, command classes in the model. Controller package has basis for communicating with server. Thank you for the Netpaint project code, GD!

---
+++ Dispatch/src/model/Dispatch_command.java
@@ -0,0 +1,18 @@
+package model;
+
+import controller.Dispatch;
+
+public class Dispatch_command extends Command<Dispatch>{
+
+	public Dispatch_command(String source) {
+		super(source);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute(Dispatch executeOn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
