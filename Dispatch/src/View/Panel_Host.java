1ed616a20887888f0c68d18fb0f199fc821401cd
Kristoffer Cabulong <kcabulon@email.arizona.edu>
2015-03-19 14:14:46 -0700

    -Created individual clients (CICOClient, SchedulerClient) so that they could be worked on separately. -Created empty class for CompleteClient which will have JTabbed Dispatch, CICO, and Scheduler

--- Dispatch/src/View/Panel_Host.java
+++ Dispatch/src/View/Panel_Host.java
@@ -3,6 +3,11 @@ package View;
 import javax.swing.JPanel;
 
 public class Panel_Host extends JPanel{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8158953088490839171L;
+
 	public Panel_Host() {
 	}
 
---
+++ Dispatch/src/controller/CompleteClient.java
@@ -0,0 +1,7 @@
+package controller;
+
+import javax.swing.JFrame;
+
+public class CompleteClient extends JFrame{
+
+}
