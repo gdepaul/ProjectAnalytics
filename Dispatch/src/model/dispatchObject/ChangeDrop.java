e1642bd70f6393d04c833151ed3b585fa9813c09
N R Callahan <nicholas.r.callahan@gmail.com>
2015-03-19 17:09:39 -0700

    Only required the clubs name

--- Dispatch/src/model/dispatchObject/ChangeDrop.java
+++ Dispatch/src/model/dispatchObject/ChangeDrop.java
@@ -1,20 +1,16 @@
 package model.dispatchObject;
 
 public class ChangeDrop extends DispatchObject<DispatchObject>{
-
-	public ChangeDrop(String club, int cash, int tickets, int change) {
-		super(club, cash, tickets, change);
-		// TODO Auto-generated constructor stub
-	}
+	private static final long serialVersionUID = 2715653630216322407L;
+	
+	public ChangeDrop(String club) {
+		this.club=club;	}
 	
 	@Override
 	public String getObjectType(){
 		return "ChangeDrop";
 	}
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2715653630216322407L;
+
 
 }
