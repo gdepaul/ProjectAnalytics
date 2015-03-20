ed547338c3c6c264893a2021eb647377d4f94dc1
N R Callahan <nicholas.r.callahan@gmail.com>
2015-03-19 17:10:22 -0700

    white space

--- Dispatch/src/model/Club.java
+++ Dispatch/src/model/Club.java
@@ -13,7 +13,8 @@ public class Club implements Serializable {
 	String clubName;
 	float money;
 	int tickets;
-	int cashdrops;
+	int changedrops=0;
+	int cashdrops=0;
 	int fullsheets;
 	int halfsheets;
 	int singletickets;
@@ -65,6 +66,7 @@ public class Club implements Serializable {
 		return tickets;
 	}
 	
+	public void putChangeDrop() { this.changedrops++; }
 	public void putCashDrop() { this.cashdrops++; }
 	public void putFullSheet(int amount) { this.fullsheets += amount; }
 	public void putHalfSheet(int amount) { this.halfsheets += amount; }
