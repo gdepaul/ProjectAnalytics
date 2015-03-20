28ced7f39416efb04f9d6f3b5177128cf90475ef
N R Callahan <nicholas.r.callahan@gmail.com>
2015-03-19 17:08:48 -0700

    Now can pull Half, Full, or single tiuckets from the class

--- Dispatch/src/model/dispatchObject/TicketDrop.java
+++ Dispatch/src/model/dispatchObject/TicketDrop.java
@@ -2,8 +2,15 @@ package model.dispatchObject;
 
 public class TicketDrop extends DispatchObject<DispatchObject> {
 
-	public TicketDrop(String club, int cash, int tickets, int change) {
-		super(club, cash, tickets, change);
+	private int full;
+	private int half;
+	private int single;
+	
+	public TicketDrop(String club, int full, int half, int single) {
+		this.club   = club;
+		this.full   = full;
+		this.half   = half;
+		this.single = single;
 	}
 	
 	@Override
@@ -11,9 +18,10 @@ public class TicketDrop extends DispatchObject<DispatchObject> {
 		return "TicketDrop";
 	}
 
-	/**
-	 * 
-	 */
+	public int getFullSheets()    { return full;   }
+	public int getHalfSheets()    { return half;   }
+	public int getSingleTickets() { return single; }
+	
 	private static final long serialVersionUID = -7907417359925041211L;
 
 }
