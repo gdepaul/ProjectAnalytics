6eb4d77abb9ec2151b1bbf3ca5eeef18e84eaf16
N R Callahan <nicholas.r.callahan@gmail.com>
2015-03-29 20:41:36 -0700

    Updated SaveFile so it uses to correct information (HashMaps vs lists)

--- Dispatch/src/server/SaveFile.java
+++ Dispatch/src/server/SaveFile.java
@@ -4,25 +4,30 @@ import java.io.Serializable;
 import java.util.*;
 
 import model.Club;
+import model.FieldSupervisor;
 import model.dispatch.Dispatch;
 
 public class SaveFile implements Serializable {
 
-	private HashMap<String,Club> clubs;
-	private List<String> availableFS;
-	private List<String> dispatchedFS;
-	
+	private HashMap<String,Club> activeClubs;
+	private HashMap<String, Club> inactiveClubs;
+	private HashMap<String, FieldSupervisor> field_sups;
 	private Map<String, Deque<Dispatch<DispatchServer>>> history;
 
 	
-	public SaveFile(HashMap<String,Club> clubs, List<String> avail, List<String> disp, Map<String, Deque<Dispatch<DispatchServer>>> hist) {
-		this.clubs = clubs;
-		this.availableFS = avail;
-		this.dispatchedFS = disp;
+	public SaveFile(HashMap<String,Club> clubs, HashMap<String,Club> inactiveClubs, HashMap<String, FieldSupervisor> field_sups, Map<String, Deque<Dispatch<DispatchServer>>> hist) {
+		this.activeClubs = clubs;
+		this.inactiveClubs = inactiveClubs;
+		this.field_sups = field_sups;
 		this.history = hist;
 	}
+	public SaveFile() {
+		
+	}
 	
-	public HashMap<String,Club> getClubs() { return clubs; }
-	public List<String> getAvailableFS()   { return this.availableFS; }
-	public List<String> getDispatchedFS()  { return this.dispatchedFS; }
+	public HashMap<String,Club> getClubs() { return this.activeClubs; }
+	public HashMap<String, Club> getInactiveClubs() { return this.inactiveClubs; }
+	public HashMap<String, FieldSupervisor> getFieldSupervisors()   { return this.field_sups; }
+	public Map<String, Deque<Dispatch<DispatchServer>>> getHistory() { return this.history; }
+	
 }
