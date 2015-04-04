acf032cd30edfb9f7d8e67c7274fcfbc80b4d222
N R Callahan <nicholas.r.callahan@gmail.com>
2015-04-04 12:00:26 -0700

    Added ability to save/retrieve booth in savefile

--- Dispatch/src/server/SaveFile.java
+++ Dispatch/src/server/SaveFile.java
@@ -3,6 +3,7 @@ package server;
 import java.io.Serializable;
 import java.util.*;
 
+import model.Booth;
 import model.Club;
 import model.FieldSupervisor;
 import model.dispatch.Dispatch;
@@ -12,14 +13,16 @@ public class SaveFile implements Serializable {
 	private HashMap<String,Club> activeClubs;
 	private HashMap<String, Club> inactiveClubs;
 	private HashMap<String, FieldSupervisor> field_sups;
+	private HashMap<String, Booth> booths;
 	private Map<String, Deque<Dispatch<DispatchServer>>> history;
 
 	
-	public SaveFile(HashMap<String,Club> clubs, HashMap<String,Club> inactiveClubs, HashMap<String, FieldSupervisor> field_sups, Map<String, Deque<Dispatch<DispatchServer>>> hist) {
+	public SaveFile(HashMap<String,Club> clubs, HashMap<String,Club> inactiveClubs, HashMap<String, FieldSupervisor> field_sups, Map<String, Deque<Dispatch<DispatchServer>>> hist, HashMap<String, Booth> booths) {
 		this.activeClubs = clubs;
 		this.inactiveClubs = inactiveClubs;
 		this.field_sups = field_sups;
 		this.history = hist;
+		this.booths = booths;
 	}
 	public SaveFile() {
 		
@@ -28,6 +31,6 @@ public class SaveFile implements Serializable {
 	public HashMap<String,Club> getClubs() { return this.activeClubs; }
 	public HashMap<String, Club> getInactiveClubs() { return this.inactiveClubs; }
 	public HashMap<String, FieldSupervisor> getFieldSupervisors()   { return this.field_sups; }
-	public Map<String, Deque<Dispatch<DispatchServer>>> getHistory() { return this.history; }
-	
+	public HashMap<String, Booth> getBooths() { return this.booths; }
+	public Map<String, Deque<Dispatch<DispatchServer>>> getHistory() { return this.history; }	
 }
