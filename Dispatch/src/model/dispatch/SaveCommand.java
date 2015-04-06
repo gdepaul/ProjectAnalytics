dc2e8c6311e0701621819db96e24a83a3306252e
N R Callahan <nicholas.r.callahan@gmail.com>
2015-04-05 17:45:39 -0700

    Made serializable

--- Dispatch/src/model/dispatch/SaveCommand.java
+++ Dispatch/src/model/dispatch/SaveCommand.java
@@ -6,6 +6,11 @@ import server.DispatchServer;
 public class SaveCommand extends Dispatch<DispatchServer>  {
 
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6144651262350033393L;
+
 	public SaveCommand(String source) {
 		super(source);
 	}
